06dca86b18ed620d5ae32860ab1e44b3e0996afb
RanjanGowdaJ <ranjantj1996@gmail.com>
2022-04-05 17:02:55 +0530

    commit

---
+++ src/test/java/Feature/login.feature
@@ -0,0 +1,9 @@
+Feature:
+
+
+Scenario: As a user i want to login to application
+
+Given as a user i will launch the browse
+And I will enter the url
+When enter the username and password
+Then i will validate data home page is dispalyed
---
+++ src/test/java/Stepdefinition/Login.java
@@ -0,0 +1,33 @@
+package Stepdefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login {
+	@Given("as a user i will launch the browse")
+	public void as_a_user_i_will_launch_the_browse() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("I will enter the url")
+	public void i_will_enter_the_url() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("enter the username and password")
+	public void enter_the_username_and_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("i will validate data home page is dispalyed")
+	public void i_will_validate_data_home_page_is_dispalyed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+
+}
